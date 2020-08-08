56d5ef1420b6c13f104bb58548a3a2a12e1dc7da
Pradeep A M <caruppu@gmail.com>
2020-08-08 17:06:10 +0100

    v1

---
+++ force-app/main/default/aura/inputLookup/inputLookupController.js
@@ -0,0 +1,57 @@
+({
+	searchJS : function(component, event, helper) {
+        try
+        {
+            var searchText = component.get("v.LookupSearchText");
+            if(searchText != null && searchText != ''){
+            	helper.search(component);
+            }else{
+                $A.util.removeClass(component.find("combo"), 'slds-is-open');
+            }
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    },
+    
+    selectRecordJS : function(component, event, helper) {
+		var combo = component.find("combo");
+        var searchBox = component.find("searchBox");
+        var searchPill = component.find("searchPill");
+        $A.util.addClass(searchBox, 'slds-hide');
+        $A.util.removeClass(searchBox, 'slds-show');
+        $A.util.addClass(searchPill, 'slds-show');
+        $A.util.removeClass(searchPill, 'slds-hide');
+        $A.util.removeClass(combo, 'slds-is-open');
+        
+        var dataSelectedItem = event.currentTarget.getAttribute("data-selectedItem");
+        var SearchResultLst = component.get("v.SearchResultList");
+        for(var i=0; i<SearchResultLst.length; i++){
+            if(SearchResultLst[i].Id == dataSelectedItem){
+                component.set("v.LookupSelectedRecord", SearchResultLst[i]);
+                component.set("v.LookupSelectedRecordId", SearchResultLst[i].Id);
+                break;
+            }
+        }
+	},
+    
+    removeSelectionJS : function(component, event, helper) {
+        try
+        {
+            component.set("v.LookupSearchText", null);
+            component.set("v.LookupSelectedRecord", null);
+            component.set("v.LookupSelectedRecordId", null);
+            var searchBox = component.find("searchBox");
+            var searchPill = component.find("searchPill");
+            $A.util.addClass(searchBox, 'slds-show');
+            $A.util.removeClass(searchBox, 'slds-hide');
+            $A.util.addClass(searchPill, 'slds-hide');
+            $A.util.removeClass(searchPill, 'slds-show');
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    }
+})
---
+++ force-app/main/default/aura/inputLookup/inputLookupHelper.js
@@ -0,0 +1,49 @@
+({
+	search : function(cmp) {
+        var action = cmp.get("c.searchObj");
+        
+        var f1 = cmp.get("v.LookupField_1");
+        var f2 = cmp.get("v.LookupField_2");
+        var f3 = cmp.get("v.LookupField_3");
+        
+        action.setParams({
+            'obj': cmp.get("v.LookupObject"),
+            'f1': f1,
+            'f2': f2,
+            'f3': f3,
+            'searchText': cmp.get("v.LookupSearchText"),
+            'additionalClause': cmp.get("v.LookupAdditionalClause"),
+            'rowLimit': cmp.get("v.LookupRowLimit"),
+            'withSharing': cmp.get("v.withSharing")
+        });
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") 
+            {
+                var res = response.getReturnValue();
+                if (res.length == 0) 
+                {
+                    cmp.set("v.Message", 'No Result Found...');
+                }
+                else if (res.length > 0)
+                {
+                    res.forEach(function(resultItem){
+                        resultItem.f1 = resultItem[f1];
+                        resultItem.f2 = resultItem[f2];
+                        resultItem.f3 = resultItem[f3];
+                    });
+                    cmp.set("v.SearchResultList", res);
+                    var combo = cmp.find("combo");
+                    $A.util.addClass(combo, 'slds-is-open');
+                }
+                else
+                {
+                    console.log('cLookupHelper.search: controller returned obj: ' + res);
+                }
+            }
+        });
+        
+        $A.enqueueAction(action);
+	}
+})
